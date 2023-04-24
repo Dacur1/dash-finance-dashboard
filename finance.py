f205922647e21adc7cb2336269e00404406953db
David <david281100@hotmail.com>
2023-04-24 17:58:09 +0200

    added update functions for indicator and line graph

--- finance.py
+++ finance.py
@@ -81,7 +81,7 @@ app.layout = dbc.Container([
                             dbc.Label(id='low-price', children='12.237')
                         ]),
                         dbc.Col([
-                            dbc.Label(id='low-price', children='13.418')
+                            dbc.Label(id='high-price', children='13.418')
                         ])
                     ])
                 ]),
@@ -93,9 +93,66 @@ app.layout = dbc.Container([
             width=6)
     ], justify='center'),
 
-    dcc.Interval(id='update', n_intervals=0, interval=1000*5)
+    dcc.Interval(id='update', n_intervals=0, interval=1000 * 5)
 ])
 
 
-if __name__=='__main__':
+# Indicator Graph
+@app.callback(
+    Output('indicator-graph', 'figure'),
+    Input('update', 'n_intervals')
+)
+def update_graph(timer):
+    dff_rv = dff.iloc[::-1]
+    day_start = dff_rv[dff_rv['date'] == dff_rv['date'].min()]['rate'].values[0]
+    day_end = dff_rv[dff_rv['date'] == dff_rv['date'].max()]['rate'].values[0]
+
+    fig = go.Figure(go.Indicator(
+        mode="delta",
+        value=day_end,
+        delta={'reference': day_start, 'relative': True, 'valueformat': '.2%'}))
+    fig.update_traces(delta_font={'size':12})
+    fig.update_layout(height=30, width=70)
+
+    if day_end >= day_start:
+        fig.update_traces(delta_increasing_color='green')
+    elif day_end < day_start:
+        fig.update_traces(delta_decreasing_color='red')
+
+    return fig
+
+# Line Graph
+@app.callback(
+    Output('daily-line', 'figure'),
+    Input('update', 'n_intervals')
+)
+def update_graph(timer):
+    dff_rv = dff.iloc[::-1]
+    fig = px.line(dff_rv, x='date', y='rate',
+                  range_y=[dff_rv['rate'].min(), dff_rv['rate'].max()],
+                  height=120).update_layout(margin=dict(t=0, r=0, l=0, b=20),
+                                            paper_bgcolor='rgba(0,0,0,0)',
+                                            plot_bgcolor='rgba(0,0,0,0)',
+                                            yaxis=dict(
+                                                title=None,
+                                                showgrid=False,
+                                                showticklabels=False
+                                            ),
+                                            xaxis=dict(
+                                                title=None,
+                                                showgrid=False,
+                                                showticklabels=False
+                                            ))
+
+    day_start = dff_rv[dff_rv['date'] == dff_rv['date'].min()]['rate'].values[0]
+    day_end = dff_rv[dff_rv['date'] == dff_rv['date'].max()]['rate'].values[0]
+
+    if day_end >= day_start:
+        return fig.update_traces(fill='tozeroy', line={'color': 'green'})
+    elif day_end < day_start:
+        return fig.update_traces(fill='tozeroy',
+                                 line={'color': 'red'})
+
+
+if __name__ == '__main__':
     app.run_server(debug=True, port=3000)
